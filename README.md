134deaa04f32c5c7f9b7e15b544e20af0852190b
unknown <gfiacador@gmail.com>
2021-04-28 21:07:33 -0300

    up

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# consultas
-Microserviço de clientes para a unidade curricular da USJT de sistemas distribuidos 
+# Microserviço de consultas
+
+Microserviço de consultas para a unidade curricular da USJT de sistemas distribuidos.
+
+## API Reference
+
+#### Lista todos os clientes cadastrados com os seus respectivos ingressos
+
+```http
+  GET /clientes
+```
---
+++ src/index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const axios = require("axios");
+const baseConsulta = {};
+const funcoes = {
+    ClienteCriado: (cliente) => {
+        if (Object.keys(baseConsulta).length === 0){
+            baseConsulta[cliente.contador] = cliente;
+            baseConsulta[cliente.contador]["ingressos"] = [];
+        } else if (baseConsulta[cliente.contador] != null && baseConsulta[cliente.contador]["ingressos"].length > 0){
+            const ingressos = baseConsulta[cliente.contador]["ingressos"];
+            baseConsulta[cliente.contador] = cliente;
+            baseConsulta[cliente.contador]["ingressos"] = ingressos;
+        } else {
+            baseConsulta[cliente.contador] = cliente;
+            baseConsulta[cliente.contador]["ingressos"] = [];
+        }
+    },
+    ClienteDeletado: (id) => {
+        axios.post('http://localhost:10000/eventos', {
+                tipo: "ClienteDeletadoComIngressos",
+                dados: {
+                    id: id 
+                }
+        });
+        delete baseConsulta[id];
+    }, 
+    IngressoCriado: (ingresso) => {
+        const ingressos = 
+            baseConsulta[ingresso.clienteId]["ingressos"] || [];
+            ingressos.push(ingresso);
+            baseConsulta[ingresso.clienteId]["ingressos"] = 
+            ingressos;
+            baseConsulta[ingresso.clienteId].quantIngressos = 
+            ingressos.length;
+        axios.post('http://localhost:10000/eventos', {
+                tipo: "ClienteComIngressos",
+                dados: {
+                    id: ingresso.clienteId,
+                    quant: baseConsulta[ingresso.clienteId].quantIngressos
+                }
+        });
+    },
+    IngressoDeletado: (id) => {
+        const len = baseConsulta[id]["ingressos"].length;
+        baseConsulta[id]["ingressos"].splice(len-1, 1)
+        baseConsulta[id].quantIngressos = baseConsulta[id]["ingressos"].length;
+
+        axios.post('http://localhost:10000/eventos', {
+                tipo: "ClienteComIngressos",
+                dados: {
+                    id: id,
+                    quant: baseConsulta[id].quantIngressos
+                }
+        });
+    },
+};
+
+app.get("/clientes", (req, res) => {
+    res.status(200).send(baseConsulta);
+});
+app.post("/eventos", (req, res) => {
+    try {
+        funcoes[req.body.tipo](req.body.dados);
+    } catch (err) {}
+    res.status(200).send(baseConsulta);
+});
+app.listen(6000, async() => {
+    console.log("Consultas. Porta 6000");
+});
